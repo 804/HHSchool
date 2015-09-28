76257fd6801fa062a050bfab29d0d308a9cf9169
Eugene <sujiro33@gmail.com>
2015-09-29 00:19:25 +0300

    The formation of the reverse polish notation and its calculation were made in separate methods.

--- Polynom.py
+++ Polynom.py
@@ -18,7 +18,7 @@ Output:
 import math
 
 
-def add_operand_in_stack(operand, reverse_polish, operand_stack):
+def add_operand_in_stack(operand, reverse_polish_notation, operand_stack):
     """ Adding operand at stack and pushing it out to the output
     operands from the stack while there will be nothing left, or until
     on the top of the stack will be '(', or until the top of the stack
@@ -26,15 +26,15 @@ def add_operand_in_stack(operand, reverse_polish, operand_stack):
 
     :param operand: added operand
     :type operand: <type 'str'>
-    :param reverse_polish: output in the form of reverse Polish notation
-    :type reverse_polish: <type 'list'>
+    :param reverse_polish_notation: output in the form of reverse Polish notation
+    :type reverse_polish_notation: <type 'list'>
     :param operand_stack: stack's current statement
     :type operand_stack: <type 'list'>
     """
     priority = ('+', '-', '*')
     while operand_stack and (operand_stack[len(operand_stack) - 1] != '(') and (
                 priority.index(operand_stack[len(operand_stack) - 1]) > priority.index(operand)):
-        reverse_polish.append(operand_stack.pop())
+        reverse_polish_notation.append(operand_stack.pop())
     operand_stack.append(operand)
 
 
@@ -169,66 +169,85 @@ def multiple_polinomials(first, second):
     return result
 
 
-def main():
-    expression = raw_input("\nInput expression:\n").replace(' ', '').lower()
+def make_reverse_polish_notation(expression):
+    """ The function to make reverse polish notation
+    form from input expression
+
+    :param expression: input expression
+    :type expression: <type 'str'>
+    :return: reverse polish notation form
+    :rtype: <type 'list'>
+    """
     position = 0
     numbers = [chr(item) for item in xrange(48, 58)]
-    reverse_polish = []
+    reverse_polish_notation = []
     operand_stack = []
     while position < len(expression):
         if (position != 0) and (expression[position - 1] == ')'):
             if expression[position] in (numbers + ['x', '(']):
-                add_operand_in_stack('*', reverse_polish, operand_stack)
+                add_operand_in_stack('*', reverse_polish_notation, operand_stack)
         if (position != 0) and (expression[position] == '('):
             if expression[position - 1] in (numbers + ['x']):
-                add_operand_in_stack('*', reverse_polish, operand_stack)
+                add_operand_in_stack('*', reverse_polish_notation, operand_stack)
 
         if expression[position] == '(':
             operand_stack.append('(')
             position += 1
         elif expression[position] == ')':
             while operand_stack[len(operand_stack) - 1] != '(':
-                reverse_polish.append(operand_stack.pop())
+                reverse_polish_notation.append(operand_stack.pop())
             operand_stack.pop()
             position += 1
         elif expression[position] in ('+', '-', '*'):
-            add_operand_in_stack(expression[position], reverse_polish, operand_stack)
+            add_operand_in_stack(expression[position], reverse_polish_notation, operand_stack)
             position += 1
         else:
             limits = [expression[position:].index(char) for char in ('+', '-', ')', '(', '*') if
                       char in expression[position:]]
             if limits:
-                reverse_polish.append(expression[position:position + min(limits)])
+                reverse_polish_notation.append(expression[position:position + min(limits)])
                 position += min(limits)
             else:
-                reverse_polish.append(expression[position:])
+                reverse_polish_notation.append(expression[position:])
                 position = len(expression)
     while operand_stack:
-        reverse_polish.append(operand_stack.pop())
+        reverse_polish_notation.append(operand_stack.pop())
+    return reverse_polish_notation
 
+
+def calculate(reverse_polish_notation):
+    """ Calculate expression in the form of reverse polish notation
+
+    :param reverse_polish_notation: reverse polish notation form of expression
+    :type reverse_polish_notation: <type 'list'>
+    """
     position = 0
-    reverse_polish = [string_monomial_to_dictionary(string) if string not in ('+', '-', '*') else string for string in
-                      reverse_polish]
-
-    while position < len(reverse_polish):
-        if reverse_polish[position] == '+':
-            reverse_polish[position - 2:position + 1] = [
-                addition_polinomials(reverse_polish[position - 2], reverse_polish[position - 1])]
+    while position < len(reverse_polish_notation):
+        if reverse_polish_notation[position] == '+':
+            reverse_polish_notation[position - 2:position + 1] = [
+                addition_polinomials(reverse_polish_notation[position - 2], reverse_polish_notation[position - 1])]
             position -= 1
             continue
-        elif reverse_polish[position] == '-':
-            reverse_polish[position - 2:position + 1] = [
-                subtraction_polinomials(reverse_polish[position - 2], reverse_polish[position - 1])]
+        elif reverse_polish_notation[position] == '-':
+            reverse_polish_notation[position - 2:position + 1] = [
+                subtraction_polinomials(reverse_polish_notation[position - 2], reverse_polish_notation[position - 1])]
             position -= 1
             continue
-        elif reverse_polish[position] == '*':
-            reverse_polish[position - 2:position + 1] = [
-                multiple_polinomials(reverse_polish[position - 2], reverse_polish[position - 1])]
+        elif reverse_polish_notation[position] == '*':
+            reverse_polish_notation[position - 2:position + 1] = [
+                multiple_polinomials(reverse_polish_notation[position - 2], reverse_polish_notation[position - 1])]
             position -= 1
             continue
         position += 1
 
-    print '\nResult: \n %s' % dictionary_polinomial_to_string(reverse_polish[0])
+
+def main():
+    expression = raw_input("\nInput expression:\n").replace(' ', '').lower()
+    reverse_polish_notation = make_reverse_polish_notation(expression)
+    reverse_polish_notation = [string_monomial_to_dictionary(string) if string not in ('+', '-', '*') else string for
+                               string in reverse_polish_notation]
+    calculate(reverse_polish_notation)
+    print '\nResult: \n %s' % dictionary_polinomial_to_string(reverse_polish_notation[0])
 
 
 if __name__ == '__main__':
